11141a8d830b8a36529b556c869f2a90f8ea541b
oguzhan7576 <oguzhandogru81@gmail.com>
2020-02-13 18:48:27 +0100

    Create tempmute.js

---
+++ commands/tempmute.js
@@ -0,0 +1,42 @@
+const discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, args) => {
+
+
+
+    if (!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Sorry you have no Permissions to this.");
+
+    var user = message.guild.member(message.mentions.users.firs() || message.guild.members.get(args[0]));
+
+    if (!user) return message.channel.send("Specify a user");
+
+    var muteRole = message.guild.roles.find("name", "muted");
+
+    if (!muteRole) return message.channel.send(" The role does not exist");
+
+    var muteTime = args[1];
+
+    if (!muteTime) return message.channel.send("Give a Time.⏰");
+
+    await (user.addRole(muteRole.id));
+
+    message.channel.send(`${user} is gemuted for ${muteTime}`);
+
+    setTimeout(function  () {
+        
+        user.removeRole(muteRole.id);
+
+        message.channel.send(`${user} is unmuted.`);
+
+
+
+
+    }, ms(muteTime));
+
+
+}
+
+module.exports.help = {
+    name: "tempmute"
+}
