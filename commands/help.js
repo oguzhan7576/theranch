c90d59cfca6353e486d7a14a94fd1a30f20ba69a
oguzhan7576 <oguzhandogru81@gmail.com>
2020-02-16 22:54:41 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,13 +5,13 @@ module.exports.run = async (bot, message, args) => {
 
 try{
     
-var text = "**The Ranch Command List** \n\n __The Ranch Command__ \n ``!Ping: Check Your Ping!⏳ `` \n ``!server: Check The Ranch Server Information.📂`` \n ``!update: View the bot update📡``  \n\n __The Ranch MiniGames__ \n ``!sps: <stone, paper, scissors,💎`` \n ``!8ball will I be rich later? (Get a random answer back)🎱"``;
+var text = "**The Ranch Command List** \n\n __The Ranch Command__ \n ``!Ping: Check Your Ping!⏳ `` \n ``!server: Check The Ranch Server Information.📂`` \n ``!update: View the bot update📡``  \n\n  __The Ranch MiniGames__ \n ``!sps: <stone, paper, scissors,💎`` \n ``!8ball will I be rich later? (Get a random answer back)🎱"``;
 
 message.author.send(text);
 
 message.channel.send("📝*Check your Dm!*");
 
-}catch (error){
+} catch (error) {
     message.channel.send("Something went wrong");
 }
 
