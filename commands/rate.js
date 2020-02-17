02962fcd57ae7aaac38954fa6c579c74c8b55baf
oguzhan7576 <oguzhandogru81@gmail.com>
2020-02-17 15:12:18 +0100

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -10,8 +10,8 @@ let rates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 let result = Math.floor((Math.random() * rates.length));
 
 if(ratus.user.id === message.author.id) {
-  return message.channel.send(`**${message.author.username}**, I'd give you ${result}/10<:🤣:427846193503272960>`);
-} else return message.channel.send(`I'd give **__${ratus.user.username}__** ${result}/10 <:🤣:427846193503272960>`);
+  return message.channel.send(`**${message.author.username}**, I'd give you ${result}/10 🤣`);
+} else return message.channel.send(`I'd give **__${ratus.user.username}__** ${result}/10 🤣`);
 
 }
 module.exports.help = {
