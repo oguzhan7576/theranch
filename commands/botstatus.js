b76b634ed9b35725f59852c1fe1ca8db6e85803c
oguzhan7576 <oguzhandogru81@gmail.com>
2020-02-17 15:44:50 +0100

    f
    f

--- commands/botstatus.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-exports.run = (client, message, args) => {
-      const botstatus = ['Online', 'Idle', 'Do Not Disturb', 'Invisable'];
-        const embed = new Discord.RichEmbed()
-        .addField("Bot Status: ", `${botstatus[client.status]}`);
-        message.channel.send(embed)
-}
-  
