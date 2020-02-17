decdbd1a9d245303bd4178df5a18336159666263
oguzhan7576 <oguzhandogru81@gmail.com>
2020-02-17 18:04:52 +0100

    d
    d

---
+++ commands/money.js
@@ -0,0 +1,15 @@
+exports.run = (message, bot, send) => {
+    let user = (!message.mentions.users.first()) ? message.author : message.mentions.users.first();
+    let mon = bot.profile.getMoney(user.id);
+    send("**:atm: | " + user.username + " has " + mon + " :dollar:**")
+  }
+  
+  exports.conf = {
+    userPerm: [],
+    botPerm: ["SEND_MESSAGES"],
+    coolDown: 0,
+    dm: true,
+    category: "Social",
+    help: "Get your current balance",
+    args: "",
+  }
