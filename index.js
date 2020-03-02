9e91c7e876e9e90b81b50bc9ba86d11b7ac0b665
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-02 20:39:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ bot.on("ready", async () => {
 
 console.log(`${bot.user.username} is online!`)
 
-bot.user.setActivity("The Ranch | !help", {type: "WATCHING"});
+bot.user.setActivity("The Ranch | !help", {type: "STREAMING"});
 
 })
 
