ccd6c84dad3659043d6307a0aaa9c868c3a2d20e
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-05 23:36:16 +0100

    fd
    f

--- commands/hold.js
+++ commands/hold.js
@@ -4,7 +4,7 @@ const snekfetch = require("snekfetch")
 exports.run = async (client, message, args) => {
       if (message.mentions.users.size < 1) return message.channel.send("you can't hold hands with nobody")
       let user = message.guild.member(message.mentions.users.first());
-            message.channel.send(`${user} **You held hands with** ${message.author.username} 👮`,{
+            message.channel.send(`${user} **You held hands with** ${message.author.username} 👫`,{
                 embed: {
                     image: {
                         url: "https://media.giphy.com/media/TnUJHKyjwHXOM/giphy.gif"
