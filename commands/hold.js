80607d79a1ee9b977b955647bc4ff34fc21c1c4e
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-05 23:32:06 +0100

    f
    f

--- commands/hold.js
+++ commands/hold.js
@@ -2,16 +2,16 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const snekfetch = require("snekfetch")
 exports.run = async (client, message, args) => {
-      if (message.mentions.users.size < 1) return message.channel.send("you can't tackle nobody")
+      if (message.mentions.users.size < 1) return message.channel.send("you can't hold hands with nobody")
       let user = message.guild.member(message.mentions.users.first());
-            message.channel.send(`${user} **You got a tackle from** ${message.author.username} 🧲`,{
+            message.channel.send(`${user} **You held hands with** ${message.author.username} 👮`,{
                 embed: {
                     image: {
-                        url: "http://gifimage.net/wp-content/uploads/2017/07/anime-tackle-hug-gif-12.gif"
+                        url: "https://media.giphy.com/media/TnUJHKyjwHXOM/giphy.gif"
                     }
                 }
             })
 }
 module.exports.help = {
-    name: "tackle"
+    name: "hold"
 }
