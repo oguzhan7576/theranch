38485b0f857a3631b0bf178b240af8e7085ad345
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-06 19:23:44 +0100

    f
    ff

---
+++ commands/coinflip.js
@@ -0,0 +1,11 @@
+  
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+exports.run = (client, message, args) => {
+      var coinflip = ['Heads!','Tails!'];
+      message.channel.send(coinflip[Math.floor(Math.random () * coinflip.length)]);
+}
+
+module.exports.help = {
+    name: "coinflip"
+}
