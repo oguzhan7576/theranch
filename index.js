b06ba963c6b9195ba493b685a6ab29ddcc093280
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-10 18:49:30 +0100

    g
    g

--- index.js
+++ index.js
@@ -154,29 +154,24 @@ if(command === `${prefix}ban`) {
     
         return;
     
+ }
 
-
-        bot.on("guildMemberAdd", member => {
+ bot.on("guildMemberAdd", member => {
  
-            const channel = member.guild.channels.find("name", "👑welcome-log");
-            if (!channel) console.log("Kan het kanaal niet vinden.");
-         
-            var joinEmbed = new discord.RichEmbed()
-                .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
-                .setDescription(`Hoi ${member.user.username}, **Welcome to  The Ranch server**. type *help* in #bot-commands for more information.`)
-                .setColor("#00FF00")
-                .setTimestamp()
-                .setFooter("Gebruiker gejoined.");
-         
-            channel.send(joinEmbed);
-         
-        });
-
-}
-
-
+    const channel = member.guild.channels.find("name", "report");
+    if (!channel) console.log("Kan het kanaal niet vinden.");
+ 
+    var joinEmbed = new discord.RichEmbed()
+        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
+        .setDescription(`Hoi ${member.user.username}, **Welkom op de server**. Hier nog meer uitleg.`)
+        .setColor("#00FF00")
+        .setTimestamp()
+        .setFooter("Gebruiker gejoined.");
+ 
+    channel.send(joinEmbed);
+ 
+});
 
 });
 
-
-bot.login(process.env.token); 
+bot.login(process.env.token);
