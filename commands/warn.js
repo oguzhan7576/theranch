1673dc70c36606b317a7d08978bf51277d2f339c
oguzhan7576 <oguzhandogru81@gmail.com>
2020-03-11 16:09:14 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -32,7 +32,6 @@ module.exports.run = async (bot, message, args) => {
 var warnEmbed = new discord.RichEmbed()
 .setDescription("Warn")
 .setColor("#eb4034")
-.setThumbnail(icon)
 .setAuthor("User Name", message.author.username)
 .addField("Warned member", user)
 .addField("Warned by", message.author)
